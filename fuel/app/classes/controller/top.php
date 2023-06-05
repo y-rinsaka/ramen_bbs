9cf3ce59058fcb518d1f323b641bb5cacf216f6e
y-rinsaka <yrinsaka@gmail.com>
2023-06-05 20:56:48 +0900

    first commit

---
+++ fuel/app/classes/controller/top.php
@@ -0,0 +1,18 @@
+<?php
+class Controller_Top extends Controller
+{
+    public function action_index()
+    {
+        $query= DB::select()->from('prefectures');
+        $results=$query->execute();
+    
+        $view= 'top';
+        $twig= View_Twig::forge($view);
+        $presenter= Presenter::forge($view, 'view', null, $twig);
+        $presenter-> set('results', $results);
+    
+        return Response::forge($presenter);
+
+    }
+}
+
---
+++ fuel/app/classes/presenter/top.php
@@ -0,0 +1,5 @@
+<?php
+
+class Presenter_Top extends Presenter{
+
+}
