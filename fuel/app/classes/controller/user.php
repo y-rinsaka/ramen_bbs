b0a3b2a696b80d2c14f6000bf06e887cf019214f
y-rinsaka <yrinsaka@gmail.com>
2023-06-28 22:55:52 +0900

    view weekly ramen record

--- fuel/app/classes/controller/user.php
+++ fuel/app/classes/controller/user.php
@@ -10,9 +10,39 @@ class Controller_User extends Controller
         $result = $query->execute()->as_array();
         $data['user'] = $result[0];
         
-        // そのユーザーが投稿した数
+        // そのユーザーの総投稿数
         $data['user']['ramen_post_count'] = Model_RamenPost::count('id', true, array('user_id'=>$user_id));
 
+        // 7日間の記録
+        // 直近の一週間の開始日と終了日を取得
+        $start_date = strtotime('-1 week');
+        $endDate = time();
+
+
+        $target_date = date("n/j"); // 今日の日付
+        $start_date = date("Y-m-d", strtotime($target_date . " -6 day")); // 開始日（今日から6日前）
+        $current_date = $start_date;
+
+        $records = [];
+        for ($i = 0; $i < 7; $i++) {
+            $date = date("n/j", strtotime($current_date));
+            $count = 0; // 初期値を0とする
+
+            // 該当日の投稿数を取得する
+            $query = DB::select(DB::expr('COUNT(*) as count'))
+            ->from('ramen_posts')
+            ->where('user_id', '=', 1)
+            ->where('created_at', '=', $current_date)
+            ->execute();
+
+            // 取得した投稿数を個数として連想配列に格納する
+            $count = (int) $query->get('count', 0);
+
+            $records[$date] = $count;
+
+            $current_date = date("Y-m-d", strtotime($current_date . " +1 day"));
+        }
+        $data['records'] = $records;
         return View::forge('user/index', $data);
 	}
 
