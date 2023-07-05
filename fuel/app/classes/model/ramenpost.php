19e56e91a28dbbbc9631f3d26bd5920a787d5b53
y-rinsaka <yrinsaka@gmail.com>
2023-07-05 15:51:15 +0900

    refactor model ramenpost and prefectures.php

--- fuel/app/classes/model/ramenpost.php
+++ fuel/app/classes/model/ramenpost.php
@@ -1,22 +1,22 @@
 <?php
 namespace Model;
 class RamenPost extends \Model_Crud{
-    protected static $_table_name = 'ramen_posts';
-    protected static $_primary_key = 'id';
-    protected static $_properties = array(
-        'id',
-        'user_id',
-        'prefecture_id',
-        'shop_name',
-        'shop_url',
-        'score',
-        'image',
-        'comment',
-        'created_at',
-        'updated_at'
-      );
-    protected static $_created_at = 'created_at';
-    protected static $_updated_at = 'updated_at';
-    protected static $_mysql_timestamp = true;
+  protected static $_table_name = 'ramen_posts';
+  protected static $_primary_key = 'id';
+  protected static $_properties = array(
+    'id',
+    'user_id',
+    'prefecture_id',
+    'shop_name',
+    'shop_url',
+    'score',
+    'image',
+    'comment',
+    'created_at',
+    'updated_at'
+  );
+  protected static $_created_at = 'created_at';
+  protected static $_updated_at = 'updated_at';
+  protected static $_mysql_timestamp = true;
 }
 ?>
